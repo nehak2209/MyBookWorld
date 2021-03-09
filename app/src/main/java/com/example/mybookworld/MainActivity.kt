6011333ab882f0a29be8051bc9aa1c5e82eb821f
preranashelke <preranashelke01@gmail.com>
2021-03-09 20:19:05 +0530

    code for navigation drawer

--- app/src/main/java/com/example/mybookworld/MainActivity.kt
+++ app/src/main/java/com/example/mybookworld/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.mybookworld
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBar()
     }
+    private fun setupActionBar(){
+        setSupportActionBar(toolbar_main_activity)
+        toolbar_main_activity.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+        toolbar_main_activity.setNavigationOnClickListener {
+            //Toggle Drawer
+            toggleDrawer()
+
+        }
+    }
+    private  fun toggleDrawer(){
+
+        if(drawer_layout.isDrawerOpen(GravityCompat.START)){
+            drawer_layout.closeDrawer(GravityCompat.START)
+        }else{
+            drawer_layout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
+    }
+
 }
