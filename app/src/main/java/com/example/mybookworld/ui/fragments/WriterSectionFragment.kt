6ebae8f97bc7cf1a80027ced7358e2e19c983d42
nehak2209 <nehak2209pc@gmail.com>
2021-03-27 15:22:06 +0530

    book upload page ui is ready

--- app/src/main/java/com/example/mybookworld/ui/fragments/WriterSectionFragment.kt
+++ app/src/main/java/com/example/mybookworld/ui/fragments/WriterSectionFragment.kt
@@ -1,10 +1,11 @@
 package com.example.mybookworld.ui.fragments
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.mybookworld.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -21,6 +22,10 @@ class WriterSectionFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var mSelectedImageFileUri: Uri? = null
+    //val pdf:Int=0;
+    //lateinit var uri: Uri
+    //lateinit var mStorage: StorageReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +33,12 @@ class WriterSectionFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        //btn_upload_book.setOnClickListener(){
+          //  uploadBookPdf()
+        //}
     }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -57,4 +67,7 @@ class WriterSectionFragment : Fragment() {
                 }
             }
     }
+
+
+
 }
